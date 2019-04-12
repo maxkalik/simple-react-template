7c12bac421e74789a6251892a4e8ce13f9a57ff7
Max Kalik <maxkalik@gmail.com>
2019-04-12 11:23:45 +0300

    swithced to react hooks -> useState and useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,25 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './assets/images/logo-maxkalik.svg';
 import styles from './assets/styles/main.scss';
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { spinner: true };
+const app = () => {
+  const [spinner, setSpinner] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => setSpinner(false), 1000);
+  }, []);
+
+  if (spinner) {
+    return null;
   }
 
-  componentDidMount() {
-    setTimeout(() => this.setState({ spinner: false }), 1000);
-  }
+  return (
+    <div className={styles.main}>
+      <img className={styles.logo} src={logo} alt="logo maxkalik.com" />
+      <h1>A simple react template</h1>
+      <p>© 2019 <a href="http://www.maxkalik.com">maxkalik.com</a></p>
+    </div>
+  );
+};
 
-  render() {
-    if (this.state.spinner) {
-      return null;
-    }
-
-    return (
-      <div className={styles.main}>
-        <img className={styles.logo} src={logo} alt="logo maxkalik.com" />
-        <h1>A simple react template</h1>
-        <p>© 2019 <a href="http://www.maxkalik.com">maxkalik.com</a></p>
-      </div>
-    );
-  }
-}
+export default app;
